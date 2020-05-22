fe62304db8ad3ff3cffcd0a3326103532f480651
Rhucha Narkhedkar <57826266+RhuchaNarkhedkar@users.noreply.github.com>
2020-05-22 07:22:51 +0200

    Update BoxDao.java
    adding @Repository;

--- Boxinator/Backend/Boxinator/src/main/java/com/fort/box/dao/BoxDao.java
+++ Boxinator/Backend/Boxinator/src/main/java/com/fort/box/dao/BoxDao.java
@@ -2,9 +2,10 @@ package com.fort.box.dao;
 
 import java.sql.SQLException;
 import java.util.List;
-
+import org.springframework.stereotype.Repository;
 import com.fort.box.model.Box;
 
+@Repository
 public interface BoxDao {
 	 public List<Box> getAllBoxes() throws SQLException; 
 	public Box addBox(Box box) throws SQLException; 
